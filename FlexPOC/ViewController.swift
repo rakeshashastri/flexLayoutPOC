24683f36bdc0a2cc190a75e35e9822f89f48dbc6
Rakesha Shastri <rakesha.shastri13@gmail.com>
2021-08-05 10:27:24 +0530

    image repeat mode test

--- FlexPOC/ViewController.swift
+++ FlexPOC/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FlexLayout
+import PinLayout
 
 class ViewController: UIViewController {
     
@@ -22,6 +24,12 @@ class ViewController: UIViewController {
         return gradientView
     }()
     
+    lazy var testView = UIView(frame: CGRect(x: 0, y: 200, width: 400, height: 450))
+    lazy var imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    lazy var backgroundLayer: CALayer = {
+        let layer = CALayer()
+        return layer
+    }()
     
     
     lazy var flexView = AutoLayoutView()
@@ -29,14 +37,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(gradientView)
-        NSLayoutConstraint.activate([
-            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            gradientView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            gradientView.topAnchor.constraint(equalTo: view.topAnchor)
-        ])
-        
+//        view.addSubview(gradientView)
+        view.addSubview(imageView)
+        view.addSubview(testView)
+//        NSLayoutConstraint.activate([
+//            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            gradientView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            gradientView.topAnchor.constraint(equalTo: view.topAnchor)
+//        ])
+        testView.layer.addSublayer(backgroundLayer)
+        backgroundLayer.backgroundColor = UIColor.red.cgColor
+        addBase64Image()
         
 //        view.addSubview(flexView)
 //        flexView.frame = CGRect(x: 100, y: 100, width: 100, height: 10)
@@ -52,6 +64,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func addBase64Image() {
+        let imageData = "https://lh3.googleusercontent.com/u8yWOp5E-tNHnRjWCMhOJknhTCBk9y0u-ZVUEc4w331f7nf6NZhInesTddwuxF2JprYjpjZesO2pElIc5Erq0pUGWQ=w128-h128-e365-rj-sc0x00ffffff"
+        let urlRequest = URLRequest(url: URL(string: imageData)!)
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            DispatchQueue.main.async {
+                let image = UIImage(data: data!)
+//                let newImage = image!.resizableImage(withCapInsets: .zero, resizingMode: .tile)
+//                self.imageView.image = newImage
+                let mirroredImage = UIImage(cgImage: image!.cgImage!, scale: image!.scale, orientation: .downMirrored)
+//                let scale = image!.scale
+//                let size = image!.size
+                UIGraphicsBeginImageContext(image!.size)
+                UIGraphicsGetCurrentContext()?.draw(image!.cgImage!, in: CGRect(x: 0.0, y: 0.0, width: image!.size.width, height: image!.size.height))
+                let upsideDownImage = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                self.backgroundLayer.backgroundColor = UIColor(patternImage: upsideDownImage!).cgColor
+                self.imageView.image = upsideDownImage
+//                self.backgroundLayer.transform = CATransform3DMakeScale(1.0, -1.0, 1.0)
+            }
+        }
+        task.resume()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        backgroundLayer.frame = testView.frame
+    }
 
     /*
     // MARK: - Navigation
@@ -64,3 +104,4 @@ class ViewController: UIViewController {
     */
 
 }
+ 
--- Podfile
+++ Podfile
@@ -1,9 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '14.0'
+platform :ios, '10.0'
+
+source 'https://github.com/rakeshashastri/Toast.git'
 
 # Base for swift clean Modules
 target 'FlexPOC' do
   use_frameworks!
   pod 'FlexLayout'
   pod 'PinLayout'
+  pod 'Toast'
 end
